20dd9a43ffc0ab0ec4998b226850f4d6e1981da1
Sven Grossmann <svennergr@gmail.com>
2022-07-14 11:00:03 +0200

    added annotation support

--- src/datasource.ts
+++ src/datasource.ts
@@ -5,5 +5,7 @@ import { HackeroneOptions, HackeroneQuery } from './types';
 export class DataSource extends DataSourceWithBackend<HackeroneQuery, HackeroneOptions> {
   constructor(instanceSettings: DataSourceInstanceSettings<HackeroneOptions>) {
     super(instanceSettings);
+
+    this.annotations = {};
   }
 }
